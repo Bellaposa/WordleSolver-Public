298ddc875117cbf5828a73e80586e6165714265a
Antonio Posabella <antonioposabella91@gmail.com>
2022-01-30 19:29:08 +0100

    Update

--- WordleSolver Public/SolverVM.swift
+++ WordleSolver Public/SolverVM.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+final class SolverVM: ObservableObject {
+	
+	var searchText: String = ""
+	
+	var dictionaryPath: String {
+		Bundle.main.path(forResource: "big5", ofType: "txt") ?? .init()
+	}
+	
+	var words: [String] {
+		var _words: [String] = []
+		do {
+			let content = try String(contentsOfFile: dictionaryPath)
+			_words = content.split(separator: "\n").map(String.init)
+		} catch {
+			
+		}
+		
+		return _words
+	}
+	
+	@Published var results: [String] = []
+	
+	func searchWithWords(_ word: String) {
+//		var _results: [String] = []
+//		let wordsCharacters = word.map { $0 }
+//		for word in words {
+//			for character in wordsCharacters {
+//				if word.contains(character) {
+//					_results.append(word)
+//				}
+//			}
+//		}
+//
+//		results = _results
+	}
+}
--- WordleSolver PublicTests/WordleSolver_PublicTests.swift
+++ WordleSolver PublicTests/WordleSolver_PublicTests.swift
@@ -6,30 +6,22 @@
 //
 
 import XCTest
+@testable import WordleSolver_Public
 
 class WordleSolver_PublicTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+	var vm: SolverVM!
+    
+	override func setUpWithError() throws {
+		vm = SolverVM()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+		
+	func testDictionaryPath() {
+		XCTAssert(vm.dictionaryPath.isEmpty == false)
+	}
+	
+	func testContent() {
+		let content = vm.words
+		XCTAssert(content.count == 7910)
+	}
 }
