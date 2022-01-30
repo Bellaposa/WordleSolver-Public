f490597c76f27aa609a032c91dfd2c9fa7a4e54b
Antonio Posabella <16580320+Bellaposa@users.noreply.github.com>
2022-01-30 19:29:50 +0100

    Updated View

--- WordleSolver Public/ContentView.swift
+++ WordleSolver Public/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	@ObservedObject var vm: SolverVM = .init()
+	var body: some View {
+		let binding = Binding<String>(
+			get: { self.vm.searchText },
+			set: { self.vm.searchText = $0; self.vm.searchWithWords($0) }
+		)
+		ZStack {
+			Color.black
+			VStack {
+				Text("Wordle Solver")
+					.foregroundColor(.white)
+					.padding(.vertical, 30)
+					.font(.system(size: 40, weight: .heavy, design: .default))
+				
+				TextField("Insert character", text: binding)
+					.font(.system(size: 25, weight: .heavy, design: .default))
+					.padding(.horizontal, 20)
+				
+				
+				ScrollView(.vertical, showsIndicators: true) {
+					
+					
+					ForEach(vm.results, id: \.self) { item in
+						HStack {
+							Text(item)
+								.font(.system(size: 10, weight: .heavy, design: .default))
+						}
+					}
+				}
+				Spacer()
+			}
+		}.frame(width: 400, height: 400)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
