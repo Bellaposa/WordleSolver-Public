c95bbb099cc9a8aa31f4263c27f951fce16ae94a
Antonio Posabella <a.posabella@reply.it>
2022-01-30 18:04:08 +0100

    Initial Commit

---
+++ WordleSolver Public/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleSolver Public
+//
+//  Created by Antonio Posabella on 30/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordleSolver Public/WordleSolver_PublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleSolver_PublicApp.swift
+//  WordleSolver Public
+//
+//  Created by Antonio Posabella on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleSolver_PublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
